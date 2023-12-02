00fa67eb101603451c300c5515174d52a0a3f24d
JamesNorum <70328051+JamesNorum@users.noreply.github.com>
2023-12-01 23:02:52 -0600

    Completed my ML Model
    Used C4.5 algorithm. Passed testing
    Just need to hook it up to streamlit

--- classification_model.py
+++ classification_model.py
@@ -3,25 +3,21 @@ import pandas as pd
 import numpy as np
 from collections import Counter
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, accuracy_score
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
+from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix
 
 class Node:
-    """A Node in the Decision Tree."""
+    """
+    A Node in a Decision Tree.
 
-    def __init__(self, feature_index=None, threshold=None, left=None, right=None, *, value=None):
-        """
-        Initialization of the decision tree node.
-
-        Parameters:
-        feature_index (int): Index of the feature used for splitting.
-        threshold (float): Threshold value for splitting.
-        left (Node): Left child node.
-        right (Node): Right child node.
-        value (int): Value if the node is a leaf node.
-        """
-        self.feature_index = feature_index
+    Parameters:
+    - feature (int): The index of the feature used for splitting.
+    - threshold (float): The threshold value for splitting.
+    - left (Node): The left child node.
+    - right (Node): The right child node.
+    - value (int, optional): The class label for a leaf node.
+    """
+    def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
+        self.feature = feature
         self.threshold = threshold
         self.left = left
         self.right = right
@@ -32,208 +28,179 @@ class Node:
         return self.value is not None
 
 class DecisionTree:
-    """The CART Decision Tree."""
+    """
+    Decision Tree Classifier implementing the C4.5 algorithm.
 
+    Parameters:
+    - min_samples_split (int): The minimum number of samples required to split a node. Default value is 2.
+    - max_depth (int): The maximum depth of the tree. Default value is 100.
+    """
     def __init__(self, min_samples_split=2, max_depth=100):
-        """
-        Initialization of the Decision Tree.
-
-        Parameters:
-        min_samples_split (int): Minimum number of samples required to split a node.
-        max_depth (int): Maximum depth of the tree.
-        """
         self.min_samples_split = min_samples_split
         self.max_depth = max_depth
         self.root = None
 
-    def fit(self, X, y):
-        """Fit the decision tree model.
+    @staticmethod
+    def _entropy(y):
+        """Calculate the entropy of a dataset."""
+        hist = np.bincount(y)
+        ps = hist / len(y)
+        return -np.sum([p * np.log2(p) for p in ps if p > 0])
+
+    def _gain_ratio(self, y, X_column, split_thresh):
+        """
+        Calculate the Gain Ratio for a given split threshold.
 
         Parameters:
-        X (numpy.ndarray): Training features.
-        y (numpy.ndarray): Target values.
+        - y (Numpy Array): The target values.
+        - X_column (Numpy Array): A column from the feature matrix.
+        - split_thresh (float): The threshold to split at.
         """
-        self.root = self._build_tree(X, y)
-        self._prune(self.root, X, y)  # Pruning after building the tree
+        parent_entropy = self._entropy(y)
+        left_idxs, right_idxs = X_column < split_thresh, X_column >= split_thresh
+        n, n_left, n_right = len(y), np.sum(left_idxs), np.sum(right_idxs)
 
-    def _prune(self, node, X, y):
-            """
-            Apply post-pruning to the decision tree.
+        if n_left == 0 or n_right == 0:
+            return 0
 
-            Parameters:
-            node (Node): Current node in the decision tree.
-            X (numpy.ndarray): Training features.
-            y (numpy.ndarray): Target values.
-            """
-            if node.is_leaf_node():
-                return
+        e_left, e_right = self._entropy(y[left_idxs]), self._entropy(y[right_idxs])
+        child_entropy = (n_left / n) * e_left + (n_right / n) * e_right
 
-            # Prune left and right children first
-            if node.left is not None:
-                self._prune(node.left, X, y)
-            if node.right is not None:
-                self._prune(node.right, X, y)
+        # Calculate Information Gain
+        info_gain = parent_entropy - child_entropy
 
-            # If both children are leaf nodes, check for pruning possibility
-            if node.left.is_leaf_node() and node.right.is_leaf_node():
-                self._cost_complexity_prune(node, X, y)
+        # Calculate Split Info
+        split_info = -((n_left / n) * np.log2(n_left / n) + (n_right / n) * np.log2(n_right / n))
 
-    def _cost_complexity_prune(self, node, X, y):
+        # Gain Ratio
+        return info_gain / split_info if split_info != 0 else 0
+
+    def _best_split(self, X, y):
         """
-        Apply cost complexity pruning on a specific node.
+        Find the best split for a node.
+
         Parameters:
-        node (Node): Node to evaluate for pruning.
-        X (numpy.ndarray): Training features.
-        y (numpy.ndarray): Target values.
+        - X (Numpy Array): The feature matrix.
+        - y (Numpy Array): The target values.
         """
-        # Predictions without the split
-        predictions_without_split = [self._traverse_tree(x, node) for x in X]
-        error_without_split = np.sum(y != predictions_without_split)
-
-        # Predictions with the split
-        predictions_left = [self._traverse_tree(x, node.left) for x in X]
-        predictions_right = [self._traverse_tree(x, node.right) for x in X]
-        error_with_split = np.sum(y != predictions_left) + np.sum(y != predictions_right)
-
-        # Prune if error without split is less or equal
-        if error_without_split <= error_with_split:
-            node.left = None
-            node.right = None
-            node.feature_index = None
-            node.threshold = None
-            node.value = Counter(y).most_common(1)[0][0]
+        best_gain = -1
+        split_idx, split_thresh = None, None
+        n_features = X.shape[1]
+        for idx in range(n_features):
+            X_column = X[:, idx]
+            thresholds = np.unique(X_column)
+            for threshold in thresholds:
+                gain = self._gain_ratio(y, X_column, threshold)
+                if gain > best_gain:
+                    best_gain = gain
+                    split_idx = idx
+                    split_thresh = threshold
+        return split_idx, split_thresh
 
     def _build_tree(self, X, y, depth=0):
-        """Recursively build the decision tree.
+        """
+        Recursively build the decision tree.
 
         Parameters:
-        X (numpy.ndarray): Training features.
-        y (numpy.ndarray): Target values.
-        depth (int): Current depth of the tree.
+        - X (Numpy Array): The feature matrix.
+        - y (Numpy Array): The target values.
+        - depth (int): The current depth of the tree.
         """
-        num_samples, num_features = X.shape
-        best_feature, best_threshold = None, None
+        n_samples, n_features = X.shape
+        n_labels = len(np.unique(y))
 
-        # Check if the node is a leaf node
-        if num_samples >= self.min_samples_split and depth <= self.max_depth:
-            best_gini = float("inf")
-            for feature_index in range(num_features):
-                thresholds = np.unique(X[:, feature_index])
-                for threshold in thresholds:
-                    gini = self._gini_index(X, y, feature_index, threshold)
-                    if gini < best_gini:
-                        best_gini = gini
-                        best_feature = feature_index
-                        best_threshold = threshold
+        # stopping criteria
+        if (depth >= self.max_depth 
+                or n_labels == 1 
+                or n_samples < self.min_samples_split):
+            leaf_value = self._most_common_label(y)
+            return Node(value=leaf_value)
 
-        # Check if y is empty
-        if len(y) == 0 or best_feature is None:
-            # If y is empty or no best feature is found, return a default leaf node
-            return Node(value=0)  # Assuming binary classification with classes {0, 1}
+        split_idx, split_thresh = self._best_split(X, y)
+        left_idxs, right_idxs = X[:, split_idx] < split_thresh, X[:, split_idx] >= split_thresh
+        left = self._build_tree(X[left_idxs, :], y[left_idxs], depth + 1)
+        right = self._build_tree(X[right_idxs, :], y[right_idxs], depth + 1)
+        return Node(split_idx, split_thresh, left, right)
 
-        # Split the data
-        left_idxs, right_idxs = self._split(X[:, best_feature], best_threshold)
-        left = self._build_tree(X[left_idxs, :], y[left_idxs], depth+1)
-        right = self._build_tree(X[right_idxs, :], y[right_idxs], depth+1)
-        return Node(best_feature, best_threshold, left, right)
+    @staticmethod
+    def _most_common_label(y):
+        """
+        Find the most common label in the dataset.
+
+        Parameters:
+        - y (Numpy Array): The target values.
+        """
+        counter = Counter(y)
+        most_common = counter.most_common(1)[0][0]
+        return most_common
+    
+    def fit(self, X, y):
+        """
+        Fit the Decision Tree Classifier to the data.
+
+        Parameters:
+        - X (Numpy Array): The feature matrix.
+        - y (Numpy Array): The target values.
+        """
+        self.root = self._build_tree(X, y)
 
     def predict(self, X):
-        """Make predictions using the decision tree.
+        """
+        Predict class labels for samples in X.
 
         Parameters:
-        X (numpy.ndarray): Input features.
+        - X (Numpy Array): The feature matrix.
         """
         return np.array([self._traverse_tree(x, self.root) for x in X])
 
     def _traverse_tree(self, x, node):
-        """Traverse the tree for making a prediction.
+        """
+        Traverse the tree to find the class label for a sample.
 
         Parameters:
-        x (numpy.ndarray): Single input feature.
-        node (Node): Current node of the tree.
+        - x (Numpy Array): A single sample from the feature matrix.
+        - node (Node): The current node in the tree.
         """
         if node.is_leaf_node():
             return node.value
-        if x[node.feature_index] <= node.threshold:
+        if x[node.feature] < node.threshold:
             return self._traverse_tree(x, node.left)
-        else:
-            return self._traverse_tree(x, node.right)
-
-    def _gini_index(self, X, y, feature_index, threshold):
-        """Calculate the Gini index for a split.
-
-        Parameters:
-        X (numpy.ndarray): Training features.
-        y (numpy.ndarray): Target values.
-        feature_index (int): Feature index.
-        threshold (float): Threshold value.
-        """
-        left_idxs, right_idxs = self._split(X[:, feature_index], threshold)
-        n = len(y)
-        n_left, n_right = len(left_idxs), len(right_idxs)
-        if n_left == 0 or n_right == 0:
-            return 0
-
-        gini_left = 1.0 - sum([(np.sum(y[left_idxs] == c) / n_left) ** 2 for c in np.unique(y[left_idxs])])
-        gini_right = 1.0 - sum([(np.sum(y[right_idxs] == c) / n_right) ** 2 for c in np.unique(y[right_idxs])])
-
-        # Calculate the weighted Gini index
-        weighted_gini = (n_left / n) * gini_left + (n_right / n) * gini_right
-        return weighted_gini
-
-    def _split(self, feature_values, threshold):
-        """Split the dataset on a feature and threshold.
-
-        Parameters:
-        feature_values (numpy.ndarray): Values of the feature to split on.
-        threshold (float): Threshold value for splitting.
-        """
-        left_idxs = np.argwhere(feature_values <= threshold).flatten()
-        right_idxs = np.argwhere(feature_values > threshold).flatten()
-        return left_idxs, right_idxs
+        return self._traverse_tree(x, node.right)
 
 
 # Load the data
-data = pd.read_csv('credit-card-fraud-detection\creditcard.csv')
+df = pd.read_csv('creditcard\creditcard.csv')
 
-# Scale the data set
-scaler = MinMaxScaler()
-scaled_data_df = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
+# The function "len" counts the number of classes = 1 and saves it as an object "fraud_records"
+fraud_records = len(df[df.Class == 1])
 
-# Subsample the data such that 90& of the data is fradulent and 10% is non-fraudulent
-fraudulent = scaled_data_df[scaled_data_df['Class'] == 1]
-non_fraudulent = scaled_data_df[scaled_data_df['Class'] == 0]
+# Defines the index for fraud and non-fraud in the lines:
+fraud_indices = df[df.Class == 1].index
+not_fraud_indices = df[df.Class == 0].index
 
-# Randomly sample non-fraudulent transactions
-non_fraudulent_sample = non_fraudulent.sample(n=len(fraudulent)*9, random_state=42)
+# Randomly collect equal samples of each type:
+under_sample_indices = np.random.choice(not_fraud_indices, fraud_records, False)
+df_undersampled = df.iloc[np.concatenate([fraud_indices, under_sample_indices]),:]
+X_undersampled = df_undersampled.iloc[:,1:30]
+y_undersampled = df_undersampled.Class
+X_train, X_test, y_train, y_test = train_test_split(X_undersampled, y_undersampled, test_size = 0.3)
 
-# Combine the fraudulent and non-fraudulent samples
-subsample = pd.concat([fraudulent, non_fraudulent_sample])
-
-# Split the data into features and target
-X = subsample.drop(['Class','Time'], axis=1)
-y = subsample['Class'].values
-
-# Split into training and testing set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Convert X to NumPy arrays
+# Convert the dataframes to numpy arrays
 X_train = X_train.values
 X_test = X_test.values
 
-# Initialize the Decision Tree model
-tree = DecisionTree(min_samples_split=5, max_depth=10)  # Adjust parameters as needed
+# Create a Decision Tree Classifier
+clf = DecisionTree()
 
 # Train the model
-tree.fit(X_train, y_train)
+clf.fit(X_train, y_train)
 
-# Predictions
-predictions = tree.predict(X_test)
+# Make predictions
+y_pred = clf.predict(X_test)
 
-# Evaluation
-print("Accuracy:", accuracy_score(y_test, predictions))
-print("Classification Report:")
-print(classification_report(y_test, predictions, zero_division=0))
+# Evaluate the Classifier
+print("Classification Report:\n", classification_report(y_test, y_pred))
+print("Accuracy Score:", accuracy_score(y_test, y_pred))
 
 # def run():
 #     st.subheader("Classification Model")
