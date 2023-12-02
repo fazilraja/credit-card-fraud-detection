34273b07abc83f002b0ffae907f0a68e0fb8d705
shivpatel <shiv.s.patel@ou.edu>
2023-12-02 13:49:31 -0600

    Finished Code

--- anomaly_detection_model.py
+++ anomaly_detection_model.py
@@ -1,3 +1,11 @@
+"""
+This script contains the code for the anomaly detection model. 
+The model is built using custom isolation forest and sklearn's IsolationForest.
+The model is trained on the credit card fraud dataset from Kaggle (https://www.kaggle.com/mlg-ulb/creditcardfraud).
+The model is evaluated using accuracy, precision, recall, f1, and auc.
+The model is deployed using Streamlit (https://www.streamlit.io/).
+"""
+
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -11,7 +19,7 @@ from sklearn.ensemble import IsolationForest
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
-# Custom node class
+
 class Node:
     """
     A node class for a binary tree. 
@@ -25,7 +33,6 @@ class Node:
         self.split_feature = None
         self.split_value = None
 
-# Custom isolation tree class
 class iTree:
     """
     A class for an isolation tree. 
@@ -35,17 +42,25 @@ class iTree:
       until the height limit is reached or there is only one observation left.
     The path_length method returns the path length for a single observation.
     The path_length_helper method is a helper function for the path_length method that recursively calculates the path length for a single observation.
+    
+    Parameters:
+    height_limit: The height limit for the tree.
     """
     # Constructor
     def __init__(self, height_limit):
         self.height_limit = height_limit
         self.root = None
 
-    # Fits the model to the training data
+    """
+    Fits a single isolation tree to the training data.
+    """
     def fit(self, X):
         self.root = self.generate_tree(X, 0)
 
-    # Generates a single isolation tree using recursive splitting of the data until the height limit is reached or there is only one observation left
+    """
+    Recursively generates a single isolation tree using random splitting of the data,
+        until the height limit is reached or there is only one observation left.
+    """
     def generate_tree(self, X, height):
         if height >= self.height_limit or len(X) <= 1:
             return Node(X)
@@ -75,11 +90,18 @@ class iTree:
             root.right = self.generate_tree(right, height + 1)
             return root
         
-    # Returns the path length for a single observation
+    """
+    Returns the path length for the data.
+    """
     def path_length(self, X):
         return self.path_length_helper(X, self.root, 0)
     
-    # Helper function for path_length method that recursively calculates the path length for a single observation 
+    """
+    A helper function for the path_length method that recursively calculates the path length for a single observation.
+    Path length is calculated as the number of edges traversed from the root to a leaf node.
+    Recursively traverse the tree until a leaf node is reached.
+    Return the path length when a leaf node is reached.
+    """
     def path_length_helper(self, X, root, path_length):
         if root.left is None and root.right is None:
             return path_length
@@ -92,7 +114,6 @@ class iTree:
                 return self.path_length_helper(X, root.right, path_length + 1)
     
 
-# Custom isolation forest class
 class iForest:
     """
     A class for an isolation forest. 
@@ -102,14 +123,26 @@ class iForest:
     The predict method returns the predictions for each observation.
     The c method is a helper function for calculating c as defined in the original paper (https://cs.nju.edu.cn/zhouzh/zhouzh.files/publication/icdm08b.pdf).
     """
-    # Constructor
+    
+    """
+    Constructor for an isolation forest.
+    Parameters:
+    n_trees: The number of trees in the forest.
+    height_limit: The height limit for each tree.
+    sample_size: The sample size for each tree.
+    """
     def __init__(self, n_trees=100, height_limit=None, sample_size=None):
         self.n_trees = n_trees
         self.height_limit = height_limit
         self.sample_size = sample_size
         self.trees = []
 
-    # Fits the model to the training data
+    """
+    Fits the model to the training data.
+    Parallelizes the tree fitting process for increased speed.
+    Sets the height limit to the ceiling of the log base 2 of the sample size if the height limit is not specified.
+    Sets the sample size to the ceiling of the square root of the length of the training data if the sample size is not specified.
+    """
     def fit(self, X):
         if self.height_limit is None:
             self.height_limit = int(np.ceil(np.log2(self.sample_size)))
@@ -121,7 +154,9 @@ class iForest:
             delayed(self.fit_tree)(X) for _ in range(self.n_trees)
         )
 
-    # Fits a single tree to a random sample of the data
+    """
+    Fits a single tree to the training data.
+    """
     def fit_tree(self, X):
         sample_indices = np.random.choice(len(X), size=self.sample_size, replace=False)
         sample = X.iloc[sample_indices]
@@ -129,7 +164,11 @@ class iForest:
         tree.fit(sample)
         return tree
 
-    # Returns the anomaly score for each observation in X
+    """
+    Anomaly score for each observation. 
+    The anomaly score is calculated as 2^(-avg_path_length/c(sample_size)), where c is a function of the sample size.
+    Path length is calculated as the average path length for each observation across all trees.
+    """
     def anomaly_score(self, X):
         path_lengths = np.zeros(len(X))
         for tree in self.trees:
@@ -138,13 +177,18 @@ class iForest:
         avg_path_length = path_lengths / self.n_trees
         return 2 ** (-avg_path_length / self.c(self.sample_size))
     
-    # Returns the predictions for each observation in X based on the specified threshold 
-    # The threshold is calculated as the specified percentile of the anomaly scores for the training data 
+    """
+    Returns the predictions for each observation in X based on the specified threshold 
+    The threshold is calculated as the specified percentile of the anomaly scores for the training data
+    """
     def predict(self, scores, threshold):
         predictions = np.array([1 if score > threshold else 0 for score in scores])
         return predictions
 
-    # Helper function for calculating c
+    """
+    Returns c as defined in the original paper (https://cs.nju.edu.cn/zhouzh/zhouzh.files/publication/icdm08b.pdf).
+    If the sample size is greater than 2, c is calculated as 2 * (ln(sample_size - 1) + Euler's constant) - 2 * (sample_size - 1) / sample_size.
+    """
     def c(self, size):
         if size > 2:
             return 2 * (np.log(size - 1) + np.euler_gamma) - 2 * (size - 1) / size
@@ -153,7 +197,7 @@ class iForest:
         else:
             return 0
 
-# Custom function for calculating metrics for the data
+
 def calculate_metrics(y_true, y_pred):
     """
     Calculates accuracy, precision, recall, f1, and auc for the data using sklearn's metrics functions.
@@ -166,7 +210,7 @@ def calculate_metrics(y_true, y_pred):
     
     return acc, precision, recall, f1, auc
 
-# Custom function for plotting the confusion matrix
+
 def plot_confusion_matrix(y_true, y_pred):
     """
     Creates a confusion matrix plot using seaborn's heatmap function.
@@ -179,8 +223,10 @@ def plot_confusion_matrix(y_true, y_pred):
     ax.set_title('Confusion Matrix')
     return fig
 
-# Custom function for running the anomaly detection model
 def run():
+    """
+    Runs the anomaly detection model.
+    """
     st.title("Anomaly Detection")
 
     # Load the data
@@ -244,8 +290,11 @@ def run():
             threshold = np.percentile(model.anomaly_score(X_train), threshold_slider)
 
             # Anomaly detection for the entered transaction
-            new_score = model.anomaly_score(input_df_scaled)
-            is_anomaly = model.predict(new_score, threshold=threshold)
+            new_score = model.anomaly_score(input_df)
+            is_anomaly = new_score > threshold
+            print(f"Anomaly Score: {new_score}")
+            print(f"Threshold: {threshold}")
+            print("\n")
             
             # Calculate the execution time
             end_time = time.time()
@@ -271,25 +320,28 @@ def run():
             fig = plot_confusion_matrix(y_test, y_pred_unseen)
             st.pyplot(fig)
 
-            # Use sklearn's IsolationForest with training data for comparison with custom isolation forest
-            model_sklearn = IsolationForest(n_estimators=number_of_trees, contamination=contamination_rate)
-            model_sklearn.fit(X_train)
-            y_pred_sklearn = model_sklearn.predict(X_train)
-            y_pred_sklearn[y_pred_sklearn == 1] = 0
-            y_pred_sklearn[y_pred_sklearn == -1] = 1
-            acc, precision, recall, f1, auc = calculate_metrics(y_train, y_pred_sklearn)
-            st.write(f"Accuracy: {acc:.2f}, Precision: {precision:.2f}, Recall: {recall:.2f}, F1: {f1:.2f}, AUC: {auc:.2f}")
-            fig = plot_confusion_matrix(y_train, y_pred_sklearn)
-            st.pyplot(fig)
+            """
+            Uncomment the following code to compare the custom isolation forest with sklearn's IsolationForest
+            """
+            # # Use sklearn's IsolationForest with training data for comparison with custom isolation forest
+            # model_sklearn = IsolationForest(n_estimators=number_of_trees, contamination=contamination_rate, max_samples=256)
+            # model_sklearn.fit(X_train)
+            # y_pred_sklearn = model_sklearn.predict(X_train)
+            # y_pred_sklearn[y_pred_sklearn == 1] = 0
+            # y_pred_sklearn[y_pred_sklearn == -1] = 1
+            # acc, precision, recall, f1, auc = calculate_metrics(y_train, y_pred_sklearn)
+            # st.write(f"Accuracy: {acc:.2f}, Precision: {precision:.2f}, Recall: {recall:.2f}, F1: {f1:.2f}, AUC: {auc:.2f}")
+            # fig = plot_confusion_matrix(y_train, y_pred_sklearn)
+            # st.pyplot(fig)
 
-            # Use sklearn's IsolationForest with test data for comparison with custom isolation forest
-            y_pred_sklearn_unseen = model_sklearn.predict(X_test)
-            y_pred_sklearn_unseen[y_pred_sklearn_unseen == 1] = 0
-            y_pred_sklearn_unseen[y_pred_sklearn_unseen == -1] = 1
-            acc, precision, recall, f1, auc = calculate_metrics(y_test, y_pred_sklearn_unseen)
-            st.write(f"Accuracy: {acc:.2f}, Precision: {precision:.2f}, Recall: {recall:.2f}, F1: {f1:.2f}, AUC: {auc:.2f}")
-            fig = plot_confusion_matrix(y_test, y_pred_sklearn_unseen)
-            st.pyplot(fig)
+            # # Use sklearn's IsolationForest with test data for comparison with custom isolation forest
+            # y_pred_sklearn_unseen = model_sklearn.predict(X_test)
+            # y_pred_sklearn_unseen[y_pred_sklearn_unseen == 1] = 0
+            # y_pred_sklearn_unseen[y_pred_sklearn_unseen == -1] = 1
+            # acc, precision, recall, f1, auc = calculate_metrics(y_test, y_pred_sklearn_unseen)
+            # st.write(f"Accuracy: {acc:.2f}, Precision: {precision:.2f}, Recall: {recall:.2f}, F1: {f1:.2f}, AUC: {auc:.2f}")
+            # fig = plot_confusion_matrix(y_test, y_pred_sklearn_unseen)
+            # st.pyplot(fig)
            
         # Display an error message if the user did not enter a transaction 
         except Exception as e:
@@ -299,5 +351,5 @@ def run():
 if __name__ == "__main__":
     run()
 
-# Example transaction:
+# Example transaction input:
 #-1.359807134,-0.072781173,2.536346738,1.378155224,-0.33832077,0.462387778,0.239598554,0.098697901,0.36378697,0.090794172,-0.551599533,-0.617800856,-0.991389847,-0.311169354,1.468176972,-0.470400525,0.207971242,0.02579058,0.40399296,0.251412098,-0.018306778,0.277837576,-0.11047391,0.066928075,0.128539358,-0.189114844,0.133558377,-0.021053053,149.62
