08fe576c24a9536d2dbed51d5e4aade947d55f74
JamesNorum <70328051+JamesNorum@users.noreply.github.com>
2023-12-02 02:22:37 -0600

    Finished Code
    Still need to test StreamLit

--- classification_model.py
+++ classification_model.py
@@ -3,7 +3,10 @@ import pandas as pd
 import numpy as np
 from collections import Counter
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix
+from sklearn.metrics import classification_report, accuracy_score, roc_auc_score, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+import time
 
 class Node:
     """
@@ -32,8 +35,8 @@ class DecisionTree:
     Decision Tree Classifier implementing the C4.5 algorithm.
 
     Parameters:
-    - min_samples_split (int): The minimum number of samples required to split a node. Default value is 2.
-    - max_depth (int): The maximum depth of the tree. Default value is 100.
+    - min_samples_split (int): The minimum number of samples required to split a node. Default is 2.
+    - max_depth (int): The maximum depth of the tree. Default is 100.
     """
     def __init__(self, min_samples_split=2, max_depth=100):
         self.min_samples_split = min_samples_split
@@ -52,8 +55,8 @@ class DecisionTree:
         Calculate the Gain Ratio for a given split threshold.
 
         Parameters:
-        - y (Numpy Array): The target values.
-        - X_column (Numpy Array): A column from the feature matrix.
+        - y (NumPy Array): The target values.
+        - X_column (NumPy Array): A column from the feature matrix.
         - split_thresh (float): The threshold to split at.
         """
         parent_entropy = self._entropy(y)
@@ -80,8 +83,8 @@ class DecisionTree:
         Find the best split for a node.
 
         Parameters:
-        - X (Numpy Array): The feature matrix.
-        - y (Numpy Array): The target values.
+        - X (NumPy Array): The feature matrix.
+        - y (NumPy Array): The target values.
         """
         best_gain = -1
         split_idx, split_thresh = None, None
@@ -102,8 +105,8 @@ class DecisionTree:
         Recursively build the decision tree.
 
         Parameters:
-        - X (Numpy Array): The feature matrix.
-        - y (Numpy Array): The target values.
+        - X (NumPy Array): The feature matrix.
+        - y (NumPy Array): The target values.
         - depth (int): The current depth of the tree.
         """
         n_samples, n_features = X.shape
@@ -128,7 +131,7 @@ class DecisionTree:
         Find the most common label in the dataset.
 
         Parameters:
-        - y (Numpy Array): The target values.
+        - y (NumPy Array): The target values.
         """
         counter = Counter(y)
         most_common = counter.most_common(1)[0][0]
@@ -139,8 +142,8 @@ class DecisionTree:
         Fit the Decision Tree Classifier to the data.
 
         Parameters:
-        - X (Numpy Array): The feature matrix.
-        - y (Numpy Array): The target values.
+        - X (NumPy Array): The feature matrix.
+        - y (NumPy Array): The target values.
         """
         self.root = self._build_tree(X, y)
 
@@ -149,7 +152,7 @@ class DecisionTree:
         Predict class labels for samples in X.
 
         Parameters:
-        - X (Numpy Array): The feature matrix.
+        - X (NumPy Array): The feature matrix.
         """
         return np.array([self._traverse_tree(x, self.root) for x in X])
 
@@ -158,7 +161,7 @@ class DecisionTree:
         Traverse the tree to find the class label for a sample.
 
         Parameters:
-        - x (Numpy Array): A single sample from the feature matrix.
+        - x (NumPy Array): A single sample from the feature matrix.
         - node (Node): The current node in the tree.
         """
         if node.is_leaf_node():
@@ -167,45 +170,148 @@ class DecisionTree:
             return self._traverse_tree(x, node.left)
         return self._traverse_tree(x, node.right)
 
-# Load the data
-df = pd.read_csv('creditcard\creditcard.csv')
+def print_metrics(y_test, y_pred):
+    """
+    Evaluates and displays the performance metrics of a classifier.
 
-# The function "len" counts the number of classes = 1 and saves it as an object "fraud_records"
-fraud_records = len(df[df.Class == 1])
+    Parameters:
+    y_test (NumPy Array): True labels for the test dataset.
+    y_pred (NumPy Array): Predicted labels by the classifier for the test dataset.
 
-# Defines the index for fraud and non-fraud in the lines:
-fraud_indices = df[df.Class == 1].index
-not_fraud_indices = df[df.Class == 0].index
-
-# Randomly collect equal samples of each type:
-under_sample_indices = np.random.choice(not_fraud_indices, fraud_records, False)
-df_undersampled = df.iloc[np.concatenate([fraud_indices, under_sample_indices]),:]
-X_undersampled = df_undersampled.iloc[:,1:30]
-y_undersampled = df_undersampled.Class
-X_train, X_test, y_train, y_test = train_test_split(X_undersampled, y_undersampled, test_size = 0.3)
-
-# Convert the dataframes to numpy arrays
-X_train = X_train.values
-X_test = X_test.values
-
-# Create a Decision Tree Classifier
-clf = DecisionTree()
-
-# Train the model
-clf.fit(X_train, y_train)
-
-# Make predictions
-y_pred = clf.predict(X_test)
-
-# Evaluate the Classifier
-print("Classification Report:\n", classification_report(y_test, y_pred))
-print("Accuracy Score:", accuracy_score(y_test, y_pred))
-
-# def run():
-#     st.subheader("Classification Model")
+    The function computes and displays the following:
+    - Classification report including precision, recall, f1-score for each class.
+    - Overall accuracy of the model.
+    - AUC score representing the classifier's ability to distinguish between classes.
+    - Confusion Matrix visualized as a heatmap.
+    """
+    # Evaluation Metrics
+    print("Classification Report:\n", classification_report(y_test, y_pred))
+    print("Accuracy Score:", accuracy_score(y_test, y_pred))
+    print("AUC:", roc_auc_score(y_test, y_pred))
     
-#     # Load the dataset (replace 'creditcard.csv' with the actual path to your dataset)
-#     df = pd.read_csv('creditcard.csv')
+    # Confusion Matrix
+    cm = confusion_matrix(y_test, y_pred)
+    sns.heatmap(cm, annot=True, fmt='d', annot_kws={'size': 14})
+    plt.xlabel('Predicted Label')
+    plt.ylabel('True Label')
+    plt.title('Confusion Matrix')
+    # Labels for the quadrants
+    labels = [['TN:', 'FP:'], ['FN:', 'TP:']]
+
+    # Add text annotations for the quadrant labels
+    for i in range(2):
+        for j in range(2):
+            plt.text(j+.3 , i+.3 , labels[i][j], 
+                    horizontalalignment='center', 
+                    verticalalignment='top', 
+                    fontsize=16, color='black' if cm[i, j] > cm.max() / 2 else 'white')
+    plt.show()
+
+# # Load the data
+# df = pd.read_csv('creditcard\creditcard.csv')
+
+# # The function "len" counts the number of classes = 1 and saves it as an object "fraud_records"
+# fraud_records = len(df[df.Class == 1])
+
+# # Defines the index for fraud and non-fraud in the lines:
+# fraud_indices = df[df.Class == 1].index
+# not_fraud_indices = df[df.Class == 0].index
+
+# # Randomly collect equal samples of each type:
+# under_sample_indices = np.random.choice(not_fraud_indices, fraud_records, False)
+# df_undersampled = df.iloc[np.concatenate([fraud_indices, under_sample_indices]),:]
+# X_undersampled = df_undersampled.iloc[:,1:30]
+# y_undersampled = df_undersampled.Class
+# X_train, X_test, y_train, y_test = train_test_split(X_undersampled, y_undersampled, test_size = 0.3)
+
+# # Convert the dataframes to numpy arrays
+# X_train = X_train.values
+# X_test = X_test.values
+
+# # Create a Decision Tree Classifier
+# clf = DecisionTree()
+
+# # Train the model
+# clf.fit(X_train, y_train)
+
+# # Make predictions
+# y_pred = clf.predict(X_test)
+
+# # Evaluate the Classifier
+# print("Classification Report:\n", classification_report(y_test, y_pred))
+# print("Accuracy Score:", accuracy_score(y_test, y_pred))
+
+def run():
+    st.title("Classification Model")
     
-#     # Add code for classification model here
-#     st.write("Add your classification model code here")
+    # Load the dataset (replace 'creditcard.csv' with the actual path to your dataset)
+    filepath = '..\creditcard\creditcard.csv'
+    data = pd.read_csv(filepath)
+
+    # Create sidebar for selecting parameters
+    min_samples_split = st.sidebar.slider("Minimum number of samples required to split a node", 2, 15, 2)
+    max_depth = st.sidebar.slider("Maximum depth of the tree", 1, 100, 1)
+    user_input = st.text_area("Enter transaction features separated by commas (V1, V2, ..., V28, Amount):")
+
+    if st.button('Run Model'):
+        try:
+            # Convert user input to a numpy array and reshape for prediction
+            user_input = np.array(user_input.split(',')).astype(float).reshape(1, -1)
+            
+            if(len(user_input) != 29):
+                st.error("Please enter 29 features separated by commas.")
+                return
+
+            # Display the user input
+            st.write("User Input:")
+            st.write(user_input)
+
+            # Subsample the data such that 90& of the data is fradulent and 10% is non-fraudulent
+            fraudulent = data[data['Class'] == 1]
+            non_fraudulent = data[data['Class'] == 0]
+
+            # Randomly sample non-fraudulent transactions
+            non_fraudulent_sample = non_fraudulent.sample(n=len(fraudulent)*9, random_state=42)
+
+            # Combine the fraudulent and non-fraudulent samples
+            subsample = pd.concat([fraudulent, non_fraudulent_sample])
+
+            # Split the data into features and target
+            X = subsample.drop(['Class','Time'], axis=1)
+            y = subsample['Class'].values
+
+            # Split into training and testing set
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.9, random_state=42)
+
+            # Start the execution timer
+            start_time = time.time()
+
+            # Create a Decision Tree Classifier
+            clf = DecisionTree(min_samples_split=min_samples_split, max_depth=max_depth)
+            clf.fit(X_train, y_train)
+
+            # Make a prediction on the user input
+            user_prediction = clf.predict(user_input)
+            
+            # Calculate the time it took to run the model
+            end_time = time.time()
+
+            # Display the prediction
+            prediction_label = 'Fraudulent' if user_prediction[0] == 1 else 'Non-Fraudulent'
+            st.write(f"Predicted Class: {prediction_label}")
+            st.write(f"Execution Time: {end_time - start_time:.2f} seconds")
+
+
+            # Display the performance metrics
+            st.write("Performance Metrics:")
+            y_pred = clf.predict(X_test)
+            st.write(print_metrics(y_test, y_pred))
+
+        # Display an error message if the user did not enter a transaction 
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    run()
+
+            
